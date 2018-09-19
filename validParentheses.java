7219abd5f6950635b05cbb872a6576ccb498a984
vuochnin <vuochnin@users.noreply.github.com>
2018-09-18 21:12:56 -0700

    Create validParentheses.java

---
+++ validParentheses.java
@@ -0,0 +1,31 @@
+public boolean isValid(String s) {
+    if(s.length() == 0)
+        return true;
+    char ch = s.charAt(0);
+    if(ch == ']' || ch == ')' || ch == '}') return false;
+    Stack<Character> stack = new Stack<>();
+    for(int i = 0; i < s.length(); i++){
+        ch = s.charAt(i);
+
+        if(ch == '(' || ch == '[' || ch == '{')
+            stack.push(ch);
+        //else if(!stack.isEmpty()){
+            else if(ch == ')'){
+                if(!stack.isEmpty() && stack.peek() == '(')
+                    stack.pop();
+                else return false;
+            }
+            else if(ch == ']'){
+                if(!stack.isEmpty() && stack.peek() == '[')
+                    stack.pop();
+                else return false;
+            }
+            else if(!stack.isEmpty() && ch == '}'){
+                if(stack.peek() == '{')
+                    stack.pop();
+                else return false;
+            }
+        //}else return stack.isEmpty();
+    }
+    return stack.isEmpty();
+}
