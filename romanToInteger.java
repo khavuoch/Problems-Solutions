48dfc2cb7b72d10006ed172525a17d76418d97e1
vuochnin <vuochnin@users.noreply.github.com>
2018-09-18 21:08:09 -0700

    Update romanToInteger.java

--- romanToInteger.java
+++ romanToInteger.java
@@ -1,44 +1,44 @@
-    public int romanToInt(String s) {
-        Map map = new HashMap();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-        
-        int result = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            char value = s.charAt(i);
-            System.out.print(map.get(value));
-            //result += (int)map.get(value);
-            if(value == 'I'){
-                if((i+1 < s.length()) && (s.charAt(i+1) == 'V' || s.charAt(i+1) == 'X')){
-                    result += (int)map.get(s.charAt(i+1)) - (int)map.get(value);
-                    i++;
-                }else{
-                    result += (int)map.get(value);
-                }
+public int romanToInt(String s) {
+    Map map = new HashMap();
+    map.put('I', 1);
+    map.put('V', 5);
+    map.put('X', 10);
+    map.put('L', 50);
+    map.put('C', 100);
+    map.put('D', 500);
+    map.put('M', 1000);
+
+    int result = 0;
+
+    for(int i = 0; i < s.length(); i++){
+        char value = s.charAt(i);
+        System.out.print(map.get(value));
+        //result += (int)map.get(value);
+        if(value == 'I'){
+            if((i+1 < s.length()) && (s.charAt(i+1) == 'V' || s.charAt(i+1) == 'X')){
+                result += (int)map.get(s.charAt(i+1)) - (int)map.get(value);
+                i++;
+            }else{
+                result += (int)map.get(value);
             }
-            else if(value == 'X'){
-                if((i+1 < s.length()) && (s.charAt(i+1) == 'L' || s.charAt(i+1) == 'C')){
-                    result += (int)map.get(s.charAt(i+1)) - (int)map.get(value);
-                    i++;
-                }else{
-                    result += (int)map.get(value);
-                }
-            }
-            else if(value == 'C'){
-                if((i+1 < s.length()) && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M')){
-                    result += (int)map.get(s.charAt(i+1)) - (int)map.get(value);
-                    i++;
-                }else{
-                    result += (int)map.get(value);
-                }
-            }
-            else result += (int)map.get(value);
         }
-        return result;
+        else if(value == 'X'){
+            if((i+1 < s.length()) && (s.charAt(i+1) == 'L' || s.charAt(i+1) == 'C')){
+                result += (int)map.get(s.charAt(i+1)) - (int)map.get(value);
+                i++;
+            }else{
+                result += (int)map.get(value);
+            }
+        }
+        else if(value == 'C'){
+            if((i+1 < s.length()) && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M')){
+                result += (int)map.get(s.charAt(i+1)) - (int)map.get(value);
+                i++;
+            }else{
+                result += (int)map.get(value);
+            }
+        }
+        else result += (int)map.get(value);
     }
+    return result;
+}
